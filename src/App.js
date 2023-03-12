9d86f746dcf7a48acdad7cea21e5a72612cb175e
NurkaAmre <nurkerey30@gmail.com>
2023-03-12 17:15:15 +0600

    Implmented song component, player component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Song from './components/Song';
+import Player from './components/Player';
 
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+     <Song />
+     <Player />
     </div>
   );
 }
