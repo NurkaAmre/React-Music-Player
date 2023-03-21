b8833c92f8f8d225292391548b006d889403d18e
NurkaAmre <nurkerey30@gmail.com>
2023-03-21 20:46:18 +0600

    Implemented Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMusic } from '@fortawesome/free-solid-svg-icons';
+
+const Nav = () => {
+  return (
+    <>
+      <h1>Waves</h1>
+      <button>
+        Library
+        <FontAwesomeIcon icon={faMusic} />
+      </button>
+    </>
+  );
+};
+
+export default Nav;
