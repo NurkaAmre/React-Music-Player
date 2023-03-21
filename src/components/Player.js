44a1df82f54d4ce6ce759f3b0d05a576bde8e13d
NurkaAmre <nurkerey30@gmail.com>
2023-03-21 20:42:02 +0600

    Add nav components

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { 
   faPlay,
@@ -38,7 +38,7 @@ const Player = ({currentSong, isPlaying, setIsPlaying, audioRef, setSongInfo, so
             <p>{getTime(songInfo.currentTime)}</p>
             <input 
             min={0} 
-            max={songInfo.duration} 
+            max={songInfo.duration || 0} 
             value={songInfo.currentTime}
             onChange={dragHandler} 
             type="range" />
